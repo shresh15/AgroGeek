e0190cf6664377c5f006dbbf7918e34acb5b2fe2
shrestha <shresthachakrabortykgp@gmail.com>
2025-12-26 00:47:17 +0530

    backgrounnd videao ui

--- frontend/src/pages/BgVideo.jsx
+++ frontend/src/pages/BgVideo.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import weatherbg from "../assets/weatherbg.mp4";
 import "./BgVideo.css";
-function BgVideo() {
-  return (
-    <div className="container">
-      <video src={weatherbg} muted loop autoPlay></video>
-    </div>
-  );
-}
+const BgVideo = () => (
+  <video
+    autoPlay
+    loop
+    muted
+    className="fixed top-0 left-0 w-full h-full object-cover -z-10"
+  >
+    <source src="/bg.mp4" type="video/mp4" />
+  </video>
+);
 
 export default BgVideo;
